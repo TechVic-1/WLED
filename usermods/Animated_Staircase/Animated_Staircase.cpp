3618b92314e70714c7c4754ada243f5dd757f843
TechVic-1 <victor.martinsson@hotmail.com>
2025-03-16 22:41:14 +0100

    j

--- usermods/Animated_Staircase/Animated_Staircase.cpp
+++ usermods/Animated_Staircase/Animated_Staircase.cpp
@@ -13,11 +13,11 @@ class Animated_Staircase : public Usermod {
   private:
 
     /* configuration (available in API and stored in flash) */
-    bool enabled = false;                   // Enable this usermod
-    unsigned long segment_delay_ms = 150;   // Time between switching each segment
-    unsigned long on_time_ms       = 30000; // The time for the light to stay on
-    int8_t topPIRorTriggerPin      = -1;    // disabled
-    int8_t bottomPIRorTriggerPin   = -1;    // disabled
+    bool enabled = true;                   // Enable this usermod
+    unsigned long segment_delay_ms = 100;   // Time between switching each segment
+    unsigned long on_time_ms       = 10000; // The time for the light to stay on
+    int8_t topPIRorTriggerPin      = 7;    // disabled
+    int8_t bottomPIRorTriggerPin   = 6;    // disabled
     int8_t topEchoPin              = -1;    // disabled
     int8_t bottomEchoPin           = -1;    // disabled
     bool useUSSensorTop            = false; // using PIR or UltraSound sensor?
